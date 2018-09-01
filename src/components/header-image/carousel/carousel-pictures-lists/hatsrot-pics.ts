ce02b11d4ea7eda543ea2c6231ee92554e41626d
Tom <tomyitav@gmail.com>
2018-09-01 21:07:25 +0300

    Beautiful carousel for some project

---
+++ src/components/header-image/carousel/carousel-pictures-lists/hatsrot-pics.ts
@@ -0,0 +1,10 @@
+const hatsrotPics: Array<string> = [
+    'hatsrot/hatsrot1.jpg',
+    'hatsrot/hatsrot2.jpg',
+    'hatsrot/hatsrot3.jpg',
+    'hatsrot/hatsrot4.jpg',
+    'hatsrot/hatsrot5.jpg',
+    'hatsrot/hatsrot6.jpg',
+];
+
+export default hatsrotPics;
