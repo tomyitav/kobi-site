32b153c726921058aa996b81bc83aa07d0fabc36
Tom <tomyitav@gmail.com>
2018-09-04 00:15:00 +0300

    Colmobil was added

---
+++ pages/Colmobil.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import {Layout} from '../src/components/Layout'
+import {ContentLayout} from "../src/components/content-frames/content-layout/ContentLayout";
+import {SideBarLayout} from "../src/components/side-bars/side-bar-layout/SideBarLayout";
+import {SimpleHeaderImage} from "../src/components/header-image/simple-header-image/SimpleHeaderImage";
+import {PublicSide} from "../src/components/side-bars/public-places/PublicSide";
+import {ColmobilContent} from "../src/components/content-frames/colmobil/ColmobilContent";
+
+export default class Colmobil extends React.Component {
+  render() {
+    return (
+      <Layout>
+          <div>
+              <SimpleHeaderImage location='/static/images/colmobil.PNG'/>
+              <div className="row">
+                  <SideBarLayout header="מבני ציבור">
+                      <PublicSide selectedName="כלמוביל"/>
+                  </SideBarLayout>
+                  <ContentLayout header="פרטים">
+                      <ColmobilContent/>
+                  </ContentLayout>
+              </div>
+          </div>
+      </Layout>
+    )
+  }
+}
