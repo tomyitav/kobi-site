9f2f6aaae005eb32d5b309c8a1aa6459ff537f5b
Tom <tomyitav@gmail.com>
2018-09-04 20:08:00 +0300

    finished public places

---
+++ pages/Habira.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import {Layout} from '../src/components/Layout'
+import {ContentLayout} from "../src/components/content-frames/content-layout/ContentLayout";
+import {SideBarLayout} from "../src/components/side-bars/side-bar-layout/SideBarLayout";
+import {SimpleHeaderImage} from "../src/components/header-image/simple-header-image/SimpleHeaderImage";
+import {PublicSide} from "../src/components/side-bars/public-places/PublicSide";
+import {HabiraContent} from "../src/components/content-frames/habira/HabiraContent";
+
+export default class Habira extends React.Component {
+  render() {
+    return (
+      <Layout>
+          <div>
+              <SimpleHeaderImage location='/static/images/ulpaney.jpg'/>
+              <div className="row">
+                  <SideBarLayout header="מבני ציבור">
+                      <PublicSide selectedName="אולפני הבירה"/>
+                  </SideBarLayout>
+                  <ContentLayout header="פרטים">
+                      <HabiraContent/>
+                  </ContentLayout>
+              </div>
+          </div>
+      </Layout>
+    )
+  }
+}
---
+++ src/components/content-frames/habira/HabiraContent.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+export const HabiraContent = () =>
+    <ul>
+
+        <li>המזמין: אולפני הבירה, ערוץ 10</li>
+
+        <li>אדריכל הפרויקט: בירן אדריכלים</li>
+
+        <li>תיאור הפרויקט: בניית אולפני שידורי חדשות 10</li>
+
+        <li>היקף ביצוע: כ-5 מיליון ש"ח</li>
+
+        <li>סטטוס ביצוע: הושלם</li>
+        <br />
+
+        <a href="https://goo.gl/photos/ECTAsW1ZhtC7taEc8" target="_blank">קטלוג תמונות</a>
+
+    </ul>
