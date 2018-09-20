ec7c14fd6b224e691a7aaab776ff812a1a5cda42
Tom <tomyitav@gmail.com>
2018-09-20 12:26:43 +0300

    Improved footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,15 +4,13 @@ export const Footer = props =>
     <div>
         {/*language=PostCSS*/}
         <style jsx> {`
-        .footer {
-            color: #89898b;
+        footer {
+            color: #9999b9;
             text-align: center;
         }
       `}
         </style>
-        <nav className="navbar fixed-bottom navbar-brand">
-            <ul className="nav navbar-nav mx-auto">
-                <a className="footer" href="#">כל הזכויות שמורות לקובי ייטב</a>
-            </ul>
-        </nav>
+        <footer className="page-footer font-small blue">
+            <div className="footer-copyright text-center py-3">כל הזכויות שמורות לקובי ייטב ©</div>
+        </footer>
     </div>
