95bc3eb7b7391a25c73fda3a4d9ef796df000f2d
Tom <tomyitav@gmail.com>
2018-09-21 17:49:39 +0300

    putting icons for contact side page

--- src/components/side-bars/contact/ContactSide.tsx
+++ src/components/side-bars/contact/ContactSide.tsx
@@ -1,32 +1,43 @@
 import * as React from 'react'
 
 export const ContactSide = () =>
-    <ul className="contact-details">
+    <ul className="contact-details fa-ul">
         <style jsx>{`
             .contact-details
             {
                 padding-right: 1.1rem;
             }
 
-            #mail-content {
-                overflow: hidden;
+            .fa-ul {
+                margin-left: 0;
+            }
+
+            .fa-li {
+                right: -2em;
+                color: #7f56cd;
+            }
+
+            #mail-content{
+                // overflow: hidden;
                 text-overflow: ellipsis;
             }
         `}
         </style>
         <li id="mail-content">
-                    <span> מייל:
+            <span className="fa-li"><i className="fas fa-envelope"></i></span>
                     <a href="mailto:office1@yitav-eng.co.il"> office1@yitav-eng.co.il</a>
-                        </span>
         </li>
         <li>
-            <span>טלפון: 02-581-1882</span>
+            <span className="fa-li"><i className="fas fa-phone"></i></span>
+            <span>02-581-1882</span>
         </li>
         <li>
+            <span className="fa-li"><i className="fas fa-fax"></i></span>
             <span>פקס: 02-547-2772</span>
         </li>
         <li>
-            <span>כתובת: קיבוץ מעלה החמישה, ד.נ. הרי יהודה, 90835</span>
+            <span className="fa-li"><i className="fas fa-map-marker-alt"></i></span>
+            <span>קיבוץ מעלה החמישה, ד.נ. הרי יהודה, 90835</span>
         </li>
     </ul>
 
