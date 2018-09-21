db6d13070c31d788cc2c753faca64e5948f2ab6f
Tom <tomyitav@gmail.com>
2018-09-21 16:39:46 +0300

    fontawesome icon for side bar

--- src/components/side-bars/projects-list/ProjectsList.tsx
+++ src/components/side-bars/projects-list/ProjectsList.tsx
@@ -6,12 +6,16 @@ import Link from 'next/link'
 export const ProjectsList = (props: ProjectsListProps) => {
     const {projects, selectedName} = props
     return (
-        <ul className="project-list">
+        <ul className="project-list fa-ul">
             {/*language=PostCSS*/}
             <style jsx>{`
                     .project-list
                     {
                         padding-right: 1.1rem;
+                    }
+
+                    .fa-li {
+                        right: -2em;
                     }
                     `}
             </style>
@@ -19,6 +23,7 @@ export const ProjectsList = (props: ProjectsListProps) => {
                 projects.map((project, i) => {
                     return (
                         <li>
+                            <span className="fa-li"><i className="fas fa-angle-left"></i></span>
                             <Link href={project.location}>
                                 <a className={isSelected(project.name, selectedName)}>{project.name}</a>
                             </Link>
