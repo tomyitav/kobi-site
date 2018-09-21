e87309d86c0b4e422ad3734fe5ec4158708c4d7b
Tom <tomyitav@gmail.com>
2018-09-22 01:40:28 +0300

    Last SEO stuff, licence

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -34,6 +34,7 @@ export class Layout extends React.Component<LayoutProps, {}> {
           </style>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <meta name="google-site-verification" content="934I0ZGhTl0t_8ok6_XCfr-mwW9FrnYHrtOyRWlSB7s" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
                   integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
                   crossOrigin="anonymous"/>
