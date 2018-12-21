36d9a112c97704d00c55410be58fa4d178014dde
Tom <tomyitav@gmail.com>
2018-12-21 02:49:18 +0200

    sitemap improved

--- pages/Sitemap.tsx
+++ pages/Sitemap.tsx
@@ -12,237 +12,237 @@ const sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>1.00</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Manager</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/HatsrotHaneviim</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/YuvalGanim</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/SevenBows</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Hamagresa</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Holyland</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Contact</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.80</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Team</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/SavioneyGanArnona</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Arnona</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Park</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Marom</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Butic</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Ramot</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Mitspe</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Horesh</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/RamatDenia</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/BaitHadash</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Rasco</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/GanHair</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Maats</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Green</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Rom</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Rafa</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Elkabets</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Mishtaken</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/MishtakenYD</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Beitar</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/GoldenHill</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Nofey</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Tahun</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Hamekasher</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Novotel</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/MigdalRakevet</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Ofer</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Colmobil</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/ToratZeev</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Hevron</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Lehavim</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Buses</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Habira</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Computers</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/HarHotzvim</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/BackTeck</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/RadBinat</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '<url>\n' +
     '  <loc>https://kobiyitav.now.sh/Persis</loc>\n' +
     '  <lastmod>2018-10-19T12:32:35+00:00</lastmod>\n' +
-    '  <priority>0.64</priority>\n' +
+    '  <changefreq>weekly</changefreq>\n' +
     '</url>\n' +
     '\n' +
     '\n' +
